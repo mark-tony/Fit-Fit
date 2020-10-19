3a76ae8cc13d225e09067058af91cc2d9f93af4f
mark-tony <marktony828@gmail.com>
2020-10-19 15:11:52 -0400

    Initial Commit

---
+++ Fit+Fit/Fit_FitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fit_FitApp.swift
+//  Fit+Fit
+//
+//  Created by Mark Tony on 10/19/20.
+//
+
+import SwiftUI
+
+@main
+struct Fit_FitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
